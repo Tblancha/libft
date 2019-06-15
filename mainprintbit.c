5164a9081c25e312ff8ff09a12d9eb1e8c0c0c30
Thibaud BLANCHARD <tblancha@e3r4p1.42.fr>
2019-06-15 10:54:32 +0200

    main printbit avec plus d'info

--- mainprintbit.c
+++ mainprintbit.c
@@ -6,20 +6,19 @@
 /*   By: tblancha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 03:48:06 by tblancha          #+#    #+#             */
-/*   Updated: 2019/06/14 04:06:32 by tblancha         ###   ########.fr       */
+/*   Updated: 2019/06/15 10:51:52 by tblancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int		mainprintbit(int ac, char **av)
+int		main(int ac, char **av)
 {
 	int bin;
 	int newtab;
 	int tab;
 
-	ac += 0;
 	if (ac != 3)
 	{
 		printf("besoin de deux arguments: deux valeurs int");
@@ -27,21 +26,44 @@ int		mainprintbit(int ac, char **av)
 	}
 	bin = ft_atoi(av[1]);
 	tab = ft_atoi(av[2]);
-	printf("1er arg\n");
+
+	printf("1er arg: %d\n", bin);
 	ft_printbit(bin);
-	printf("2eme arg\n");
+
+	printf("2eme arg: %d\n", tab);
 	ft_printbit(tab);
+
 	newtab = bin & tab;
-	printf("& and exclusif\n");
+	printf("& and exclusif: %d & %d = %d\n", bin, tab, newtab);
 	ft_printbit(newtab);
+
 	newtab = bin | tab;
-	printf("| ou exclusif\n");
+	printf("| ou exclusif: %d | %d = %d\n", bin, tab, newtab);
 	ft_printbit(newtab);
+
 	newtab = bin ^ tab;
-	printf("^ xor\n");
+	printf("^ xor: %d ^ %d = %d\n", bin, tab, newtab);
 	ft_printbit(newtab);
+
 	newtab = ~bin;
-	printf("~ tild\n");
+	printf("~ tild: ~%d = %d\n", bin, newtab);
 	ft_printbit(newtab);
+
+	newtab = bin << 1;
+	printf("<< decalage: %d << 1 = %d\n", bin, newtab);
+	ft_printbit(newtab);
+
+	newtab = bin << 4;
+	printf("<< decalage: %d << 4 = %d\n", bin, newtab);
+	ft_printbit(newtab);
+
+	newtab = bin >> 1;
+	printf(">> decalage: %d >> 1 = %d\n", bin, newtab);
+	ft_printbit(newtab);
+
+	newtab = bin >> 4;
+	printf(">> decalage: %d >> 4 = %d\n", bin, newtab);
+	ft_printbit(newtab);
+
 	return (0);
 }
